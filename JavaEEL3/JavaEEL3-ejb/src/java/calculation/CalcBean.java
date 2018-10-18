18ce9298097a805953bd483a7fb2294da8a74473
Wojciech Mazur <wojciech.mazur@gorrion.pl>
2018-10-18 14:13:29 +0200

    Added remote

---
+++ JavaEEL3/JavaEEL3-ejb/src/java/calculation/CalcBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculation;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author wojte
+ */
+@Stateless
+public class CalcBean implements CalcBeanRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
